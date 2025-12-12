9689ccdb7afb80f4054b2dfffa71f99700c18277
sandeep-9983 <sandeepnagavarapu83@gmail.com>
2025-12-12 21:01:39 +0530

    Time: 10 ms (19.05%) | Memory: 42.1 MB (28.29%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        //sandeep
+        if(n==1||n==0)
+        return n;
+        int fib1=fib(n-1);
+        int fib2=fib(n-2);
+        int s=fib1+fib2;
+        return s;
+
+        
+    }
+}
