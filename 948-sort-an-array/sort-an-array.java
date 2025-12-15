e0878f31f56543608fadf5457dd15ace196d6787
sandeep-9983 <sandeepnagavarapu83@gmail.com>
2025-12-15 12:06:37 +0530

    Time: 26 ms (67.25%) | Memory: 86.7 MB (8.52%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergesort(nums,0,nums.length-1);
+        return nums;
+    }
+        public static void mergesort(int nums[],int si ,int ei)
+        {
+            if(si>=ei){
+                return;
+            }
+            int mid=si+(ei-si)/2;
+            mergesort(nums,si,mid);
+            mergesort(nums,mid+1,ei);
+            merge(nums,si,mid,ei);
+        }
+        public static void merge(int nums[],int si,int mid,int ei){
+            int temp[]=new int[ei-si+1];
+            int i=si;
+            int j=mid+1;
+            int k=0;
+            while(i<=mid&&j<=ei){
+                if(nums[i]<=nums[j]){
+                 temp[k++]=nums[i++];
+            }else{
+                temp[k++]=nums[j++];
+            }
+            }
+            while(i<=mid){
+                temp[k++]=nums[i++];
+            }
+            while(j<=ei){
+                temp[k++]=nums[j++];
+            }
+            for(k=0,i=si;k<temp.length;k++,i++){
+                nums[i]=temp[k];
+            }
+          }  }
