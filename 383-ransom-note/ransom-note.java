870a5eb83e5a823a8bbc12f210818cfe041de1c5
sandeep-9983 <sandeepnagavarapu83@gmail.com>
2026-08-01 19:53:43 +0530

    Time: 1 ms (99.98%) | Memory: 46.5 MB (57.15%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int count[]=new int[26];
-        for(int i=0;i<magazine.length();i++){
-            char ch=magazine.charAt(i);
-            int index=ch-'a';
-            count[index]++;
-        }
-        for(char ch:ransomNote.toCharArray()){
-            if(count[ch-'a']==0){
+         int freq1[]=new int[26];
+         int freq2[]=new int[26];
+         for(int i=0;i<ransomNote.length();i++){
+            char c=ransomNote.charAt(i);
+            freq1[c-'a']++;
+         }
+          for(int i=0;i<magazine.length();i++){
+            char c=magazine.charAt(i);
+            freq2[c-'a']++;
+         }
+         for(int i=0;i<26;i++){
+            if(freq1[i]>freq2[i]){
                 return false;
             }
-            count[ch-'a']--;
-        }
-        return true;
-        
+         }
+         return true;
     }
 }
